e5c0d695afc168e2af06262afde5fe4de4f48ab1
PracticeRY <rudyush905@gmail.com>
2026-04-27 18:01:30 +0530

    Update1

--- backend/models/item.js
+++ backend/models/item.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+const mongoose = ('mongoose');
 const ItemSchema = new mongoose.Schema({
 name: { type: String, required: true },
 description: { type: String, required: true },
